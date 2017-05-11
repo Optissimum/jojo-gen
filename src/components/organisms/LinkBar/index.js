a49686378be11dd463a5c34014b9203068b4e211
Sebastian MacDonald <optissimum@gmail.com>
2017-05-11 19:48:32 -0400

    Updated LinkBar object with flex

--- src/components/organisms/LinkBar/index.js
+++ src/components/organisms/LinkBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { font, palette } from 'styled-theme'
@@ -19,20 +19,32 @@ const StyledButton = styled(Button)`
   flex: none;
 `
 
-const LinkBar = (props) => {
-  return (
-    <form>
-      <Wrapper {...props}>
-        <Field name='url' label='Video Url' style={{ flex: 3 }} />
-        <Field name='end' label='Freeze Frame' style={{ flex: 1 }} />
-        <StyledButton type='submit' >Jojo Me</StyledButton>
-      </Wrapper>
-    </form>
-  )
+class LinkBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      url: this.props.url,
+      timestamp: this.props.timestamp,
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <Wrapper {...this.props}>
+          <Field name='url' label='Video Url' style={{ flex: 4 }} onChange={e => (this.setState({ url: e.target.value }))} />
+          <Field name='end' label='Freeze Frame' style={{ flex: 1 }} onChange={e => (this.setState({ timestamp: e.target.value }))} />
+          <StyledButton type='submit' >Jojo Me</StyledButton>
+        </Wrapper>
+      </form>
+    )
+  }
 }
 
 LinkBar.propTypes = {
-  reverse: PropTypes.bool,
+  url: PropTypes.string,
+  timestamp: PropTypes.number,
 }
 
 export default LinkBar
